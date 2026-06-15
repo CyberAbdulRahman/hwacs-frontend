78dc166ab5d5a00729c265f2008b2a62eb93c25b
ARahmanIrfan <abdulrahmanmohammadirfan@gmail.com>
2026-06-15 22:46:52 +0500

    Use deployed backend URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+ baseURL: import.meta.env.VITE_API_BASE_URL || "https://hwacs-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
